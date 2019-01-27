127b9c89fc779b0d5a7e00bed661a0bc7dc28587
vinnyA3 <vincent.aceto@gmail.com>
2019-01-27 16:32:07 -0500

    feat(search): implement state change

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      genres: [],
+      genres: null,
+      currentGenre: null,
     };
+
+    this.searchGenre = this.searchGenre.bind(this);
+    this.updateCurrentGenre = this.updateCurrentGenre.bind(this);
   }
+
   getGenres() {
-    //make an axios request in this component to get the list of genres from your endpoint GET GENRES
+    axios
+      .get('http://localhost:3000/genres')
+      .then(({ data }) => {
+        this.setState({ genres: data.results });
+      })
+      .catch(err => console.log(err));
+  }
+
+  updateCurrentGenre(e) {
+    this.setState({ currentGenre: e.target.value });
+  }
+
+  searchGenre() {
+    // if (this.state.currentGenre) {
+    this.props.getMovies(this.state.currentGenre);
+    // }
+  }
+
+  componentDidMount() {
+    this.getGenres();
   }
 
   render() {
@@ -23,19 +48,23 @@ class Search extends React.Component {
         </button>
         <br />
         <br />
-
-        {/* Make the select options dynamic from genres !!! */}
-        {/* How can you tell which option has been selected from here? */}
-
-        <select>
-          <option value="theway">The Way</option>
-          <option value="thisway">This Way</option>
-          <option value="thatway">That Way</option>
+        <select
+          value={this.state.currentGenre ? this.state.currentGenre : ''}
+          onChange={this.updateCurrentGenre}
+        >
+          {this.state.genres
+            ? this.state.genres.map(({ name, id }) => (
+                <option key={id} value={id}>
+                  {name}
+                </option>
+              ))
+            : ''}
         </select>
+        {/* How can you tell which option has been selected from here? */}
         <br />
         <br />
 
-        <button>Search</button>
+        <button onClick={this.searchGenre}>Search</button>
       </div>
     );
   }
