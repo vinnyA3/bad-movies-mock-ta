c7bce791c526cb3342f359c6ff92f4ac173777e1
vinnyA3 <vincent.aceto@gmail.com>
2019-01-27 16:35:31 -0500

    feat(model): create mongoose model

--- server/models/movieModel.js
+++ server/models/movieModel.js
@@ -1,9 +1,17 @@
 //SELECT one db to work with
 //For SQL
-const sqlDb = require('../../db/sql');
+// const sqlDb = require('../../db/sql');
 //For Mongo
-const mongoDb = require('../../db/mongodb')
+// const mongoDb = require('../../db/mongodb');
 
-module.exports = {
+const mongoose = require('mongoose');
 
-}
+const Movies = new mongoose.Schema({
+  movieId: Number,
+  title: String,
+  overview: String,
+  voteAverage: Number,
+  posterPath: String,
+});
+
+module.exports = mongoose.model('Movie', Movies);
