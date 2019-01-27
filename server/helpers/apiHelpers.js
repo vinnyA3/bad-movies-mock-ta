f4535f18ca2321da3baec0155ce3f73b8c05c499
vinnyA3 <vincent.aceto@gmail.com>
2019-01-27 16:33:42 -0500

    feat(apiHelpers): add get helpers

--- server/helpers/apiHelpers.js
+++ server/helpers/apiHelpers.js
@@ -10,3 +10,17 @@ const { API_KEY } = require('../../config.js');
 // Get your API Key and save it in your config file
 
 // Don't forget to export your functions and require them within your server file
+module.exports = {
+  getGenres: () =>
+    axios.get(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
+    ),
+  getMovies: ({ genre_id }) =>
+    genre_id
+      ? axios.get(
+          `https://api.themoviedb.org/3/discover/movie?with_genres=${genre_id}&primary_release_date.lte=2018-1-1&sort_by=vote_average.asc&api_key=${API_KEY}`
+        )
+      : axios.get(
+          `https://api.themoviedb.org/3/discover/movie?primary_release_date.lte=2018-1-1&sort_by=vote_average.asc&api_key=${API_KEY}`
+        ),
+};
